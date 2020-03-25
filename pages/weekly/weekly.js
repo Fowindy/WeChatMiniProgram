e4f4a3ca028f458ffa619a116bd6f04bfb58d173
Fowindy <470026339@qq.com>
2020-03-25 10:00:04 +0800

    $按电影索引显示推荐

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -1,12 +1,28 @@
 Page({
   data:{
-    thisWeekMovie:{
-        name:"教父",
-        comment:"最精彩的剧本,最真实的黑帮电影.",
-        imagePath:"/images/jf.jpg",
-        isHighlyRecommended:true
+    weeklyMovieList:[
+    {
+      name:"教父",
+      comment:"最精彩的剧本,最真实的黑帮电影",
+      imagePath:"/images/jf.jpg",
+      isHighlyRecommended:true
+
     },
+    {
+      name:"泰坦尼克号",
+      comment:"失去的才是永恒的",
+      imagePath:"/images/titanic.jpg",
+      isHighlyRecommended:false
+    },
+    {
+      name:"这个杀手不太冷",
+      comment:"小萝莉与怪叔叔纯真浪漫的爱情故事",
+      imagePath:"/images/zgssbtl.jpg",
+      isHighlyRecommended:false
+    }
+  ],
     count:123,
     score:61
   }
 })
+/**假如博主三周推荐的是这三部电影， */
