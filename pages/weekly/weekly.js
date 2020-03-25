e01a3601806dbf29c63f5ac4d2f05bd24c06efb4
Fowindy <470026339@qq.com>
2020-03-25 08:06:25 +0800

    $3.4.列表渲染_图片文字左右排版         列表渲染，重复的渲染生成组件，         wx:for属性

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -4,7 +4,7 @@ Page({
         name:"教父",
         comment:"最精彩的剧本,最真实的黑帮电影.",
         imagePath:"/images/jf.jpg",
-        isHighlyRecommended:false
+        isHighlyRecommended:true
     },
     count:123,
     score:61
