447387887712b47ac514db813fb8ab43d55627b7
Fowindy <470026339@qq.com>
2020-03-26 11:47:54 +0800

    通过this.setData将获取的页面id保存到内部数据变量mid中

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,6 +2,10 @@
 Page({
     //获取detail页在初始化时获取本次打开被指定的页面参数id
     onLoad:function(option) {
-        console.log(option.id)
+        // console.log(option.id)
+        //通过this.setData将获取的页面id保存到内部数据变量mid中
+        this.setData({
+            mid:option.id
+        })
     }
 })
