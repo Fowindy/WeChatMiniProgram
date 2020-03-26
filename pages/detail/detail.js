3ad03380d445491bd63f641663d42bf11c7a70d7
Fowindy <470026339@qq.com>
2020-03-26 16:30:59 +0800

    [小程序后台-开发-开发设置-服务器域名]中登记域名

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -10,7 +10,7 @@ Page({
         //演示:添加wx.request方法调用
         wx.request({
             //来指定请求的网址
-            url: 'https://baidu.com/a/b.jsp',
+            url: 'https://api.douban.com/v2/movie',
             //指定请求使用何种方法
             method: "GET",
             //在请求中捎带数据参数传输给Server
@@ -23,7 +23,9 @@ Page({
             },
             //当小程序接收到服务器返回的response后该如何处理_通过对应回调函数的指令完成的
             success: function (res) {
-                
+                //打印res结果返回不存在request合法域名列表
+                //[小程序后台-开发-开发设置-服务器域名]中登记域名
+                console.log(res)
             },
             fail: function () {
 
